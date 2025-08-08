5f70fef937e0946b9f5de837fb3df85c49a11f3c
EDragoev <edragoev@protonmail.com>
2025-08-08 10:16:05 -0400

    Improved the script

--- switch.bat
+++ switch.bat
@@ -1,7 +1,7 @@
 @echo off
-:: This script switches between Java versions on Windows
+REM This script switches between Java versions on Windows
 
-:: Define the paths to your Java installations
+REM Define the paths to your Java installations
 set JAVA_8=C:\Program Files\Java\jdk1.8.0_251
 set JAVA_11=C:\Program Files\Java\jdk-11.0.10
 set JAVA_17=C:\Program Files\Java\jdk-17
@@ -10,16 +10,18 @@ set JAVA_17=C:\Program Files\Java\jdk-17
 REM This script switches between Java versions on Windows
 
 REM Define the paths to your Java installations
-set JAVA_8=C:\Program Files\Java\jdk1.8.0_251
-set JAVA_11=C:\Program Files\Java\jdk-11.0.10
-set JAVA_17=C:\Program Files\Java\jdk-17
+set JAVA_8=C:\Program Files\Eclipse Adoptium\jdk-8.0.462.8-hotspot
+set JAVA_11=C:\Program Files\Eclipse Adoptium\jdk-11.0.28.6-hotspot
+set JAVA_17=C:\Program Files\Eclipse Adoptium\jdk-17.0.16.8-hotspot
+set JAVA_21=C:\Program Files\Eclipse Adoptium\jdk-21.0.8.9-hotspot
 
 REM Prompt for the Java version choice
 echo Select Java version to switch to:
 echo 1. Java 8
 echo 2. Java 11
 echo 3. Java 17
-set /p choice=Enter your choice (1, 2, or 3):
+echo 4. Java 21
+set /p choice=Enter your choice (1, 2, 3 or 4):
 
 REM Set the JAVA_HOME and update the PATH based on the choice
 if "%choice%"=="1" (
@@ -31,6 +33,9 @@ if "%choice%"=="1" (
 ) else if "%choice%"=="3" (
     echo Switching to Java 17...
     set JAVA_HOME=%JAVA_17%
+) else if "%choice%"=="4" (
+    echo Switching to Java 21...
+    set JAVA_HOME=%JAVA_21%
 ) else (
     echo Invalid choice. Exiting.
     exit /b
@@ -42,5 +47,3 @@ set PATH=%JAVA_HOME%\bin;%PATH%
 REM Print the new Java version to confirm
 echo Java version switched to:
 "%JAVA_HOME%\bin\java" -version
-
-pause
