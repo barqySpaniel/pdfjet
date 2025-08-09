44219f4b74eec09e88ac4859faabcfa8f0d4830b
EDragoev <edragoev@protonmail.com>
2025-08-09 11:46:58 -0400

    Now the script works as it should

--- run-dotnet.sh
+++ run-dotnet.sh
@@ -13,18 +13,8 @@ rm -rf obj
 # Build the PDFjet library
 dotnet build PDFjet.csproj -c release
 
-# Check if the example number is less than 10
-if [ $1 -lt 10 ]; then
-    # dotnet build examples/Example_0$1/Example_0$1.csproj -c release
-    # dotnet examples/Example_0$1/bin/release/net8.0/Example_0$1.dll
-    echo "hello"
-else
-    dotnet build examples/Example_$1/Example_$1.csproj -c release
-    dotnet examples/Example_$1/bin/release/net8.0/Example_$1.dll
-fi
-
-# Run the specified Example DLL
-# dotnet examples/$EXAMPLE/bin/release/net8.0/$EXAMPLE.dll
+dotnet build examples/Example_$1/Example_$1.csproj -c release
+dotnet examples/Example_$1/bin/release/net8.0/Example_$1.dll
 
 # Open the generated PDF for the specified Example
-# evince Example_01.pdf
+evince Example_$1.pdf
