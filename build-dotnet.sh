095da6623f25eacfa04f5744f9424a3f7618b9f3
EDragoev <edragoev@protonmail.com>
2025-08-09 10:30:04 -0400

    Started working on this change

--- build-dotnet.sh
+++ build-dotnet.sh
@@ -1,17 +1,13 @@
 # Very important!!
-./clean.sh
+rm -rf bin
+rm -rf obj
 
-for i in $(seq 1 50);
-do
-    if [ $i -lt 10 ]; then
-        rm -rf obj
-        dotnet build PDFjet.csproj /p:StartupObject=Example_0$i
-        dotnet run
-        echo ""
-    else
-        rm -rf obj
-        dotnet build PDFjet.csproj /p:StartupObject=Example_$i
-        dotnet run
-        echo ""
-    fi
-done
+dotnet build PDFjet.csproj -c release
+
+dotnet build examples/Example_01/Example_01.csproj -c release
+dotnet build examples/Example_02/Example_02.csproj -c release
+
+dotnet examples/Example_01/bin/release/net8.0/Example_01.dll
+dotnet examples/Example_02/bin/release/net8.0/Example_02.dll
+
+evince Example_02.pdf
