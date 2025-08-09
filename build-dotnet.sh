a8fd0686271decdccd6bea9170004ae44d97a022
EDragoev <edragoev@protonmail.com>
2025-08-09 11:28:35 -0400

    Now all examples work!

--- build-dotnet.sh
+++ build-dotnet.sh
@@ -10,9 +10,18 @@ dotnet build PDFjet.csproj -c release
 # Loop through Example_01 to Example_50
 for i in {1..50}
 do
+    # Check if the example number is less than 10
+    if [ $i -lt 10 ]; then
+        # For Example_01 to Example_09, prepend 0
+        example_num="Example_0$i"
+    else
+        # For Example_10 to Example_20, no need to prepend 0
+        example_num="Example_$i"
+    fi
+
     # Build each Example project
-    dotnet build examples/Example_0$i/Example_0$i.csproj -c release
+    dotnet build examples/$example_num/$example_num.csproj -c release
 
     # Run each Example DLL
-    dotnet examples/Example_0$i/bin/release/net8.0/Example_0$i.dll
+    dotnet examples/$example_num/bin/release/net8.0/$example_num.dll
 done
