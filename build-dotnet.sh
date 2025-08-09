9c0ed7819b34fc807e1ae54efae8a311245cf9b5
EDragoev <edragoev@protonmail.com>
2025-08-09 12:06:51 -0400

    Made all the tasks to run in parallel

--- build-dotnet.sh
+++ build-dotnet.sh
@@ -7,21 +7,24 @@ rm -rf obj
 # Build PDFjet library project
 dotnet build PDFjet.csproj -c release
 
+# Function to build and run a specific example
+build_and_run_example() {
+    local example_num=$1
+    dotnet build examples/$example_num/$example_num.csproj -c release
+    dotnet examples/$example_num/bin/release/net8.0/$example_num.dll
+}
+
 # Loop through Example_01 to Example_50
 for i in {1..50}
 do
-    # Check if the example number is less than 10
     if [ $i -lt 10 ]; then
-        # For Example_01 to Example_09, prepend 0
         example_num="Example_0$i"
     else
-        # For Example_10 to Example_20, no need to prepend 0
         example_num="Example_$i"
     fi
-
-    # Build each Example project
-    dotnet build examples/$example_num/$example_num.csproj -c release
-
-    # Run each Example DLL
-    dotnet examples/$example_num/bin/release/net8.0/$example_num.dll
+    # Start building and running the example in the background
+    build_and_run_example $example_num &
 done
+
+# Wait for all background processes to complete
+wait
