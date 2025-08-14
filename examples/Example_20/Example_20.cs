d545a11f67761f2c36741e83630f53183de4b648
EDragoev <edragoev@protonmail.com>
2025-08-14 00:06:02 -0400

    Updated the example ...

--- examples/Example_20/Example_20.cs
+++ examples/Example_20/Example_20.cs
@@ -52,37 +52,37 @@ class Example_20 {
 
         PDFjet.NET.Path path = new PDFjet.NET.Path();
 
-        path.Add(new Point(13.0f,  0.0f));
-        path.Add(new Point(15.5f,  4.5f));
+        path.Add(new Point(12.5f,  0.0f));
+        path.Add(new Point(15.0f,  4.5f));
 
-        path.Add(new Point(18.0f,  3.5f));
-        path.Add(new Point(15.5f, 13.5f, Point.CONTROL_POINT));
-        path.Add(new Point(15.5f, 13.5f, Point.CONTROL_POINT));
-        path.Add(new Point(20.5f,  7.5f));
+        path.Add(new Point(17.5f,  3.5f));
+        path.Add(new Point(15.0f, 13.5f, Point.CONTROL_POINT));
+        path.Add(new Point(15.0f, 13.5f, Point.CONTROL_POINT));
+        path.Add(new Point(20.0f,  7.5f));
 
-        path.Add(new Point(21.0f,  9.5f));
-        path.Add(new Point(25.0f,  9.0f));
-        path.Add(new Point(24.0f, 13.0f));
-        path.Add(new Point(25.5f, 14.0f));
-        path.Add(new Point(19.0f, 19.0f));
-        path.Add(new Point(20.0f, 21.5f));
-        path.Add(new Point(13.5f, 20.5f));
-        path.Add(new Point(13.5f, 27.0f));
-        path.Add(new Point(12.5f, 27.0f));
-        path.Add(new Point(12.5f, 20.5f));
-        path.Add(new Point( 6.0f, 21.5f));
-        path.Add(new Point( 7.0f, 19.0f));
-        path.Add(new Point( 0.5f, 14.0f));
-        path.Add(new Point( 2.0f, 13.0f));
-        path.Add(new Point( 1.0f,  9.0f));
-        path.Add(new Point( 5.0f,  9.5f));
+        path.Add(new Point(20.5f,  9.5f));
+        path.Add(new Point(24.5f,  9.0f));
+        path.Add(new Point(23.5f, 13.0f));
+        path.Add(new Point(25.0f, 14.0f));
+        path.Add(new Point(18.5f, 19.0f));
+        path.Add(new Point(19.5f, 21.5f));
+        path.Add(new Point(13.0f, 20.5f));
+        path.Add(new Point(13.0f, 27.0f));
+        path.Add(new Point(12.0f, 27.0f));
+        path.Add(new Point(12.0f, 20.5f));
+        path.Add(new Point( 5.5f, 21.5f));
+        path.Add(new Point( 6.5f, 19.0f));
+        path.Add(new Point( 0.0f, 14.0f));
+        path.Add(new Point( 1.5f, 13.0f));
+        path.Add(new Point( 0.5f,  9.0f));
+        path.Add(new Point( 4.5f,  9.5f));
 
-        path.Add(new Point( 5.5f,  7.5f));
-        path.Add(new Point(10.5f, 13.5f, Point.CONTROL_POINT));
-        path.Add(new Point(10.5f, 13.5f, Point.CONTROL_POINT));
-        path.Add(new Point( 8.0f,  3.5f));
+        path.Add(new Point( 5.0f,  7.5f));
+        path.Add(new Point(10.0f, 13.5f, Point.CONTROL_POINT));
+        path.Add(new Point(10.0f, 13.5f, Point.CONTROL_POINT));
+        path.Add(new Point( 7.5f,  3.5f));
 
-        path.Add(new Point(10.5f,  4.5f));
+        path.Add(new Point(10.0f,  4.5f));
         path.SetClosePath(true);
         path.SetColor(Color.red);
         // path.SetFillShape(true);
