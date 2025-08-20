33264635db1a956d468ee5c016eb69a9ac4a0073
EDragoev <edragoev@protonmail.com>
2025-08-19 21:59:23 -0400

    Added work in progress ContentBlock

---
+++ net/pdfjet/ContentBlock.cs
@@ -0,0 +1,88 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace PDFjet.NET {
+public class ContentBlock {
+    public int ObjectNumber { get; private set; }
+    public float Width { get; }
+    public float Height { get; }
+    public byte[] StreamData => stream.ToArray();
+
+    private MemoryStream stream;
+    private Dictionary<string, int> resourceRefs;
+
+    public ContentBlock(int objectNumber, float width, float height) {
+        ObjectNumber = objectNumber;
+        Width = width;
+        Height = height;
+        stream = new MemoryStream();
+        resourceRefs = new Dictionary<string, int>();
+    }
+
+    public void SetFillColorRGB(float r, float g, float b) {
+        Write($"{r} {g} {b} rg\n");
+    }
+
+    public void SetStrokeColorRGB(float r, float g, float b) {
+        Write($"{r} {g} {b} RG\n");
+    }
+
+    public void FillRectangle(float x, float y, float w, float h) {
+        Write($"{x} {y} {w} {h} re\nf\n");
+    }
+
+    public void DrawRectangle(float x, float y, float w, float h) {
+        Write($"{x} {y} {w} {h} re\nS\n");
+    }
+
+    public void MoveTo(float x, float y) {
+        Write($"{x} {y} m\n");
+    }
+
+    public void LineTo(float x, float y) {
+        Write($"{x} {y} l\n");
+    }
+
+    public void Stroke() {
+        Write("S\n");
+    }
+
+    public void AddResource(string name, int objectNumber) {
+        resourceRefs[name] = objectNumber;
+    }
+
+    public void WriteRaw(string s) {
+        Write(s);
+    }
+
+    private void Write(string s) {
+        var bytes = Encoding.ASCII.GetBytes(s);
+        stream.Write(bytes, 0, bytes.Length);
+    }
+
+    public string ToPdfObject() {
+        var dict = new StringBuilder();
+
+        dict.AppendLine("<<");
+        dict.AppendLine("/Type /XObject");
+        dict.AppendLine("/Subtype /Form");
+        dict.AppendFormat("/BBox [0 0 {0} {1}]\n", Width, Height);
+
+        if (resourceRefs.Count > 0) {
+            dict.AppendLine("/Resources <<");
+            foreach (var kv in resourceRefs)
+                dict.AppendFormat("/{0} {1} 0 R\n", kv.Key, kv.Value);
+            dict.AppendLine(">>");
+        }
+
+        dict.AppendFormat("/Length {0}\n", stream.Length);
+        dict.AppendLine(">>");
+
+        return $"{ObjectNumber} 0 obj\n{dict}stream\n" +
+               Encoding.ASCII.GetString(StreamData) +
+               "endstream\nendobj\n";
+    }
+}
+}
