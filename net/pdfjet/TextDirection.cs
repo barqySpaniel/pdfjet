1024bd20030b0bc256926496f1f71a199cb8bc41
EDragoev <edragoev@protonmail.com>
2025-08-21 07:43:46 -0400

    Added TextDirection.cs to the project

---
+++ net/pdfjet/TextDirection.cs
@@ -0,0 +1,16 @@
+namespace PDFjet.NET {
+    /// <summary>
+    /// Specifies the text direction and writing system orientation
+    /// </summary>
+    public enum TextDirection {
+        /// <summary>
+        /// Left-to-right direction (e.g., English, French, Spanish)
+        /// </summary>
+        LeftToRight,
+        
+        /// <summary>
+        /// Right-to-left direction (e.g., Arabic, Hebrew, Persian)
+        /// </summary>
+        RightToLeft
+    }
+}
