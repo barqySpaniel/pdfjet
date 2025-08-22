2a9de40ed740521b98a9580214d8864d2f699432
EDragoev <edragoev@protonmail.com>
2025-08-22 17:26:00 -0400

    Improved the formatting ...

--- net/pdfjet/CoreFont.cs
+++ net/pdfjet/CoreFont.cs
@@ -28,19 +28,19 @@ SOFTWARE.
  */
 namespace PDFjet.NET {
 public enum CoreFont {
-        COURIER,
-        COURIER_BOLD,
-        COURIER_OBLIQUE,
-        COURIER_BOLD_OBLIQUE,
-        HELVETICA,
-        HELVETICA_BOLD,
-        HELVETICA_OBLIQUE,
-        HELVETICA_BOLD_OBLIQUE,
-        TIMES_ROMAN,
-        TIMES_BOLD,
-        TIMES_ITALIC,
-        TIMES_BOLD_ITALIC,
-        SYMBOL,
-        ZAPF_DINGBATS,
+    COURIER,
+    COURIER_BOLD,
+    COURIER_OBLIQUE,
+    COURIER_BOLD_OBLIQUE,
+    HELVETICA,
+    HELVETICA_BOLD,
+    HELVETICA_OBLIQUE,
+    HELVETICA_BOLD_OBLIQUE,
+    TIMES_ROMAN,
+    TIMES_BOLD,
+    TIMES_ITALIC,
+    TIMES_BOLD_ITALIC,
+    SYMBOL,
+    ZAPF_DINGBATS,
 }   // End of CoreFont.cs
 }   // End of namespace PDFjet.NET
