75d6f444715c2c1e8502818776516af2f091d1ee
EDragoev <edragoev@protonmail.com>
2025-08-22 19:45:12 -0400

    Made the code more clean and simple

--- Sources/PDFjet/CoreFonts/Courier.swift
+++ Sources/PDFjet/CoreFonts/Courier.swift
@@ -7,7 +7,7 @@ public enum Courier {
     public static let underlinePosition: Int16 = -100
     public static let underlineThickness: Int16 = 50
     public static let notice = "Copyright (c) 1989, 1990, 1991, 1992, 1993, 1997 Adobe Systems Incorporated. All Rights Reserved."
-    public static let metrics = Array<[Int16]>(arrayLiteral:
+    public static let metrics:  [[Int16]] = [
         [32,600],
         [33,600],
         [34,600],
@@ -232,5 +232,5 @@ public enum Courier {
         [253,600],
         [254,600],
         [255,600]
-    )
+    ]
 }
