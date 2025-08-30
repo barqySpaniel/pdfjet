25d929c565160873b92224f23c88ff3a8ae88d09
EDragoev <edragoev@protonmail.com>
2025-08-29 22:24:56 -0400

    Update the copyright notice ...

---
+++ net/pdfjet/QRCode/MaskPattern.cs
@@ -0,0 +1,29 @@
+/**
+ *  RSBlock.cs
+ *
+ *  Copyright (c) 2025 PDFjet Software
+ *  Licensed under the MIT License. See LICENSE file in the project root.
+ *
+ *  Original author: Kazuhiko Arase, 2009
+ *  URL: http://www.d-project.com/
+ *  Licensed under MIT: http://www.opensource.org/licenses/mit-license.php
+ *
+ *  The word "QR Code" is a registered trademark of
+ *  DENSO WAVE INCORPORATED
+ *  http://www.denso-wave.com/qrcode/faqpatent-e.html
+ *
+ *  Modified and adapted for use in PDFjet by PDFjet Software
+ */
+
+namespace PDFjet.NET.QRCode {
+public class MaskPattern {
+    public const int PATTERN000 = 0;
+    public const int PATTERN001 = 1;
+    public const int PATTERN010 = 2;
+    public const int PATTERN011 = 3;
+    public const int PATTERN100 = 4;
+    public const int PATTERN101 = 5;
+    public const int PATTERN110 = 6;
+    public const int PATTERN111 = 7;
+}
+}   // End of namespace PDFjet.NET
