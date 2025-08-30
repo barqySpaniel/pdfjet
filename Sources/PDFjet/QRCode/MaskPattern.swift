b8eb09a482e2c4d576ec43f5dbcb93ba48687150
EDragoev <edragoev@protonmail.com>
2025-08-29 22:21:49 -0400

    Updated ...

--- Sources/PDFjet/QRCode/MaskPattern.swift
+++ Sources/PDFjet/QRCode/MaskPattern.swift
@@ -1,16 +1,19 @@
 /**
+ *  MaskPattern.cs
  *
-Copyright 2009 Kazuhiko Arase
-
-URL: http://www.d-project.com/
-
-Licensed under the MIT license:
-  http://www.opensource.org/licenses/mit-license.php
-
-The word "QR Code" is registered trademark of
-DENSO WAVE INCORPORATED
-  http://www.denso-wave.com/qrcode/faqpatent-e.html
-*/
+ *  Copyright (c) 2025 PDFjet Software
+ *  Licensed under the MIT License. See LICENSE file in the project root.
+ *
+ *  Original author: Kazuhiko Arase, 2009
+ *  URL: http://www.d-project.com/
+ *  Licensed under MIT: http://www.opensource.org/licenses/mit-license.php
+ *
+ *  The word "QR Code" is a registered trademark of
+ *  DENSO WAVE INCORPORATED
+ *  http://www.denso-wave.com/qrcode/faqpatent-e.html
+ *
+ *  Modified and adapted for use in PDFjet by PDFjet Software
+ */
 
 class MaskPattern {
     public static let PATTERN000 = 0
