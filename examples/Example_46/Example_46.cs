d2180b8c181d12404be68b96265027474675d113
EDragoev <edragoev@protonmail.com>
2025-09-07 14:06:20 -0400

    Simplified the example

--- examples/Example_46/Example_46.cs
+++ examples/Example_46/Example_46.cs
@@ -13,52 +13,16 @@ public class Example_46 {
             new FileStream("Example_46.pdf", FileMode.Create)));
         pdf.SetCompliance(Compliance.PDF_UA_1);
 
-        pdf.SetEncryption(new PDFEncryption(pdf, "hello", "world"));
+        // pdf.SetEncryption(new PDFEncryption(pdf, "hello", "world"));
 
-        Font f1 = new Font(pdf, IBMPlexSans.Bold);
-        f1.SetSize(14f);
-
-        Font f2 = new Font(pdf, IBMPlexSans.Regular);
-        f2.SetSize(14f);
-
-        Font f3 = new Font(pdf, JetBrainsMono.Bold);
-        f3.SetSize(14f);
+        Font f1 = new Font(pdf, CoreFont.HELVETICA);
+        f1.SetSize(36f);
 
         Page page = new Page(pdf, Letter.PORTRAIT);
 
-        List<Paragraph> paragraphs = new List<Paragraph>();
-
-        Paragraph paragraph = new Paragraph()
-                .Add(new TextLine(f1,
-"Όταν ο Βαρουφάκης δήλωνε κατηγορηματικά πως δεν θα ασχοληθεί ποτέ με την πολιτική (Video)"));
-        paragraphs.Add(paragraph);
-
-        paragraph = new Paragraph()
-                .Add(new TextLine(f2,
-"Τις τελευταίες μέρες αδιαμφισβήτητα ο Γιάνης Βαρουφάκης είναι  ο πιο πολυσυζητημένος πολιτικός στην Ευρώπη και όχι μόνο. Κι όμως, κάποτε ο νέος υπουργός Οικονομικών δήλωνε κατηγορηματικά πως δεν πρόκειται ποτέ να εμπλακεί σε αυτό το χώρο."));
-        paragraphs.Add(paragraph);
-
-        paragraph = new Paragraph()
-                .Add(new TextLine(f2,
-"Η συγκεκριμένη του δήλωση ήταν σε συνέντευξή που είχε παραχωρήσει στην εκπομπή «Ευθέως» και στον Κωνσταντίνο Μπογδάνο, όταν στις 13 Δεκεμβρίου του 2012 ο νυν υπουργός Οικονομικών δήλωνε ότι δεν υπάρχει περίπτωση να ασχοληθεί με την πολιτική, γιατί θα τον διέγραφε οποιοδήποτε κόμμα."))
-                .Add(new TextLine(f2,
-"Συγκεκριμένα, μετά από το 43ο λεπτό του βίντεο, ο δημοσιογράφος τον ρώτησε αν θα ασχολιόταν ποτέ με την πολιτική, με την απάντηση του κ. Βαρουφάκη να είναι κατηγορηματική: «Όχι, ποτέ, ποτέ»."));
-        paragraphs.Add(paragraph);
-
-        paragraph = new Paragraph()
-                .Add(new TextLine(f2,
-"«Μα ποτέ δεν ασχολήθηκα με την πολιτική. Ασχολούμαι ως πολίτης και ως συμμετέχων στον δημόσιο διάλογο. Και είναι κάτι που θα κάνω πάντα. Καταρχήν, αν μπω σε ένα πολιτικό κόμμα μέσα σε μία εβδομάδα θα με έχει διαγράψει, όποιο κι αν είναι αυτό», εξηγούσε τότε, ενώ πρόσθετε ότι δεν μπορεί να ακολουθήσει κομματική γραμμή."));
-        paragraphs.Add(paragraph);
-
-        paragraph = new Paragraph().Add(new TextLine(f3, "Hello, World!"));
-
-        paragraphs.Add(paragraph);
-
-        Text textArea = new Text(paragraphs);
-        textArea.SetLocation(70f, 70f);
-        textArea.SetWidth(500f);
-        textArea.SetBorder(true);
-        textArea.DrawOn(page);
+        TextLine textLine = new TextLine(f1, "Hello, World!");
+        textLine.SetLocation(100f, 100f);
+        textLine.DrawOn(page);
 
         pdf.Complete();
     }
