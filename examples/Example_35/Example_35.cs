6f765b314a1cd360afd02622d1027ec1e1d30f30
EDragoev <edragoev@protonmail.com>
2025-09-10 12:22:35 -0400

    Updated ...

--- examples/Example_35/Example_35.cs
+++ examples/Example_35/Example_35.cs
@@ -18,63 +18,6 @@ public class Example_35 {
         Font font = new Font(pdf, IBMPlexSans.Regular);
         font.SetSize(14f);
 
-//        // Font f1 = new Font(pdf, CoreFont.HELVETICA_BOLD);
-//        // f1.SetSize(8f);
-//
-//        // Font f2 = new Font(pdf, CoreFont.HELVETICA);
-//        // f2.SetSize(8f);
-//
-//        Font f1 = new Font(pdf, IBMPlexSans.Bold);
-//        f1.SetSize(8f);
-//
-//        Font f2 = new Font(pdf, IBMPlexSans.Regular);
-//        f2.SetSize(8f);
-//
-//        List<List<Point>> chartData = new List<List<Point>>();
-//
-//        List<Point> path1 = new List<Point>();
-//        path1.Add(new Point(50f, 50f).SetStartOfPath()); // .SetStrokeColor(Color.blue));
-//        path1.Add(new Point(55f, 55f));
-//        path1.Add(new Point(60f, 60f));
-//        path1.Add(new Point(65f, 58f));
-//        path1.Add(new Point(70f, 59f));
-//        path1.Add(new Point(75f, 63f));
-//        path1.Add(new Point(80f, 65f));
-//        chartData.Add(path1);
-//
-//        List<Point> path2 = new List<Point>();
-//        path2.Add(new Point(50f, 30f).SetStartOfPath().SetStrokeColor(Color.red));
-//        path2.Add(new Point(55f, 35f));
-//        path2.Add(new Point(60f, 40f));
-//        path2.Add(new Point(65f, 48f));
-//        path2.Add(new Point(70f, 49f));
-//        path2.Add(new Point(75f, 53f));
-//        path2.Add(new Point(80f, 55f));
-//        chartData.Add(path2);
-//
-//        List<Point> path3 = new List<Point>();
-//        path3.Add(new Point(50f, 80f).SetStartOfPath().SetStrokeColor(Color.green));
-//        path3.Add(new Point(55f, 70f));
-//        path3.Add(new Point(60f, 60f));
-//        path3.Add(new Point(65f, 55f));
-//        path3.Add(new Point(70f, 59f));
-//        path3.Add(new Point(75f, 63f));
-//        path3.Add(new Point(80f, 61f));
-//        chartData.Add(path3);
-//
-//        Chart chart = new Chart(f1, f2);
-//        chart.SetData(chartData);
-//        chart.SetLocation(70f, 50f);
-//        chart.SetSize(500f, 300f);
-//        chart.SetTitle("Chart Title");
-//        chart.SetXAxisTitle("X Axis Title");
-//        chart.SetYAxisTitle("Y Axis Title");
-//
-//        // You can adjust the X and Y min and max manually:
-//        // chart.SetXAxisMinMax(45f, 80f, 7);
-//        // chart.SetYAxisMinMax(20f, 80f, 6);
-//        chart.DrawOn(page);
-
 /*
         Rect rect = new Rect(0f, 0f, 300f, 300f);
         rect.SetLocation(0f, 0f);
@@ -103,6 +46,18 @@ public class Example_35 {
         Rect rect = new Rect(0f, 0f, 400f, 400f);
         rect.SetFillColor(Color.gray);
         container.Add(rect);
+
+        XObject xObject = new XObject(pdf, 400f, 400f);
+        xObject.SetStrokeColor(Color.darkgreen);
+        xObject.SetStrokeWidth(5f);
+        xObject.MoveTo(0f, 0f);
+        xObject.LineTo(0f, 400f);
+        xObject.LineTo(400f, 400f);
+        xObject.LineTo(400f, 0f);
+        xObject.ClosePath();
+        xObject.Complete();
+        xObject.DrawOn(page);
+        // container.Add(xObject);
 
         // Add a text line to container
         TextLine title = new TextLine(font, "Container");
