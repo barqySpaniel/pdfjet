a2851a7fc9b199c6ff68aa453c30216a45f16746
EDragoev <edragoev@protonmail.com>
2025-09-12 14:06:01 -0400

    Fixed a typo

--- com/pdfjet/PathOperator.java
+++ com/pdfjet/PathOperator.java
@@ -1,7 +1,7 @@
 public class PathOperator {
     public static final String STROKE = "S";                                // Stroke the path
     public static final String CLOSE_AND_STROKE = "s";                      // Close and then stroke the path
-    public static final String FILL = "f";                                  // Close ant fill the path
+    public static final String FILL = "f";                                  // Close and fill the path
     public static final String FILL_AND_STROKE = "b";                       // Close, fill and then stroke the path
     public static final String FILL_USING_EVEN_ODD_RULE = "f*";             // Like 'f' but using even odd rule
     public static final String FILL_USING_EVEN_ODD_RULE_AND_STROKE = "b*";  // Like 'b' but using even odd rule
--- net/pdfjet/PathOperator.cs
+++ net/pdfjet/PathOperator.cs
@@ -2,7 +2,7 @@ namespace PDFjet.NET {
 public static class PathOperator {
     public static readonly string Stroke = "S";                         // Stroke the path
     public static readonly string CloseAndStroke = "s";                 // Close and then stroke the path
-    public static readonly string Fill = "f";                           // Close ant fill the path
+    public static readonly string Fill = "f";                           // Close and fill the path
     public static readonly string FillAndStroke = "b";                  // Close, fill and then stroke the path
     public static readonly string FillUsingEvenOddRule = "f*";          // Like 'f' but using even odd rule
     public static readonly string FillUsingEvenOddRuleAndStroke = "b*"; // Like 'b' but using even odd rule
