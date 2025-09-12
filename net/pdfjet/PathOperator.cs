364b6ad2f9152af9f7d1a225549cd15bf42e02b6
EDragoev <edragoev@protonmail.com>
2025-09-12 12:29:44 -0400

    Added the PathOperator.cs class to the project

---
+++ net/pdfjet/PathOperator.cs
@@ -0,0 +1,8 @@
+public static class PathOperator {
+    public static readonly string Stroke = "S";                         // Stroke the path
+    public static readonly string CloseAndStroke = "s";                 // Close and then stroke the path
+    public static readonly string Fill = "f";                           // Close ant fill the path
+    public static readonly string FillAndStroke = "b";                  // Close, fill and then stroke the path
+    public static readonly string FillUsingEvenOddRule = "f*";          // Like 'f' but using even odd rule
+    public static readonly string FillUsingEvenOddRuleAndStroke = "b*"; // Like 'b' but using even odd rule
+}
