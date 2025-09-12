6bf1f80ad1f53042af9ccb475b84620c2e8a42a9
EDragoev <edragoev@protonmail.com>
2025-09-12 12:35:39 -0400

    Update the code ...

--- com/pdfjet/PathOperator.java
+++ com/pdfjet/PathOperator.java
@@ -1,8 +1,8 @@
 public class PathOperator {
-    public static final String Stroke = "S";                         // Stroke the path
-    public static final String CloseAndStroke = "s";                 // Close and then stroke the path
-    public static final String Fill = "f";                           // Close ant fill the path
-    public static final String FillAndStroke = "b";                  // Close, fill and then stroke the path
-    public static final String FillUsingEvenOddRule = "f*";          // Like 'f' but using even odd rule
-    public static final String FillUsingEvenOddRuleAndStroke = "b*"; // Like 'b' but using even odd rule
+    public static final String STROKE = "S";                                // Stroke the path
+    public static final String CLOSE_AND_STROKE = "s";                      // Close and then stroke the path
+    public static final String FILL = "f";                                  // Close ant fill the path
+    public static final String FILL_AND_STROKE = "b";                       // Close, fill and then stroke the path
+    public static final String FILL_USING_EVEN_ODD_RULE = "f*";             // Like 'f' but using even odd rule
+    public static final String FILL_USING_EVEN_ODD_RULE_AND_STROKE = "b*";  // Like 'b' but using even odd rule
 }
