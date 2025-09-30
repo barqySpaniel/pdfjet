856b93293861afdf14161029afd66b6e53cfa39d
EDragoev <edragoev@protonmail.com>
2025-09-30 14:55:25 -0400

    Updated ...

--- net/pdfjet/OCG.cs
+++ net/pdfjet/OCG.cs
@@ -4,13 +4,12 @@ namespace PDFjet.NET {
 public class OCG {
     // Fields to hold object number and name
     internal int objNumber;
-    internal string name;
+    internal String name;
 
     // Constructor to initialize the OCG object with objNumber and name
-    internal OCG(int objNumber, string name) {
+    internal OCG(int objNumber, String name) {
         this.objNumber = objNumber;
         this.name = name;
     }
 }
 }
-
