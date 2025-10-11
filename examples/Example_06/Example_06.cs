c5666b3cc4678c2fcc3449ab841f24715545a7d4
EDragoev <edragoev@protonmail.com>
2025-10-11 19:07:15 -0400

    Added more code ...

--- examples/Example_06/Example_06.cs
+++ examples/Example_06/Example_06.cs
@@ -96,6 +96,11 @@ public class Example_06 {
         textLine.SetURIAction("https://pdfjet.com");
         textLine.DrawOn(page);
 
+        PopupAnnotation popupAnnotation = new PopupAnnotation();
+        popupAnnotation.SetLocation(50f, 500f);
+        popupAnnotation.SetSize(50f, 50f);
+        popupAnnotation.DrawOn(page);
+
         pdf.Complete();
     }
 
--- net/pdfjet/PopupAnnotation.cs
+++ net/pdfjet/PopupAnnotation.cs
@@ -8,8 +8,16 @@ using System;
 
 namespace PDFjet.NET {
 public class PopupAnnotation : IDrawable {
+    internal String uri = null;
+    internal String key = null;
+    internal String language = null;
+    internal String actualText = null;
+    internal String altDescription = null;
+
     private float x = 0f;
     private float y = 0f;
+    private float w = 0f;
+    private float h = 0f;
 
     public PopupAnnotation() {
     }
@@ -24,7 +32,23 @@ public class PopupAnnotation : IDrawable {
         this.y = y;
     }
 
+    public void SetSize(float w, float h) {
+        this.w = w;
+        this.h = h;
+    }
+
     public float[] DrawOn(Page page) {
+        page.AddAnnotation(new Annotation(
+                Annotation.Popup,
+                uri,
+                key,    // The destination name
+                x,
+                y,
+                x + w,
+                y + h,
+                language,
+                actualText,
+                altDescription));
         return new float[] {0f, 0f};
     }
 }
