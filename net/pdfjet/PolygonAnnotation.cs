faf5b71a0a850c79b139fa3526125ef86889c118
EDragoev <edragoev@protonmail.com>
2025-10-12 09:26:07 -0400

    Added two more classes ...

---
+++ net/pdfjet/PolygonAnnotation.cs
@@ -0,0 +1,55 @@
+/**
+ * PolygonAnnotation.cs
+ *
+ * Copyright (c) 2025 PDFjet Software
+ * Licensed under the MIT License. See LICENSE file in the project root.
+ */
+using System;
+
+namespace PDFjet.NET {
+public class PolygonAnnotation : IDrawable {
+    internal String uri = null;
+    internal String key = null;
+    internal String language = null;
+    internal String actualText = null;
+    internal String altDescription = null;
+
+    private float x = 0f;
+    private float y = 0f;
+    private float w = 0f;
+    private float h = 0f;
+
+    public PolygonAnnotation() {
+    }
+
+    public void SetLocation(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void SetPosition(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void SetSize(float w, float h) {
+        this.w = w;
+        this.h = h;
+    }
+
+    public float[] DrawOn(Page page) {
+        page.AddAnnotation(new Annotation(
+                Annotation.Polygon,
+                uri,
+                key,    // The destination name
+                x,
+                y,
+                x + w,
+                y + h,
+                language,
+                actualText,
+                altDescription));
+        return new float[] {0f, 0f};
+    }
+}
+}
