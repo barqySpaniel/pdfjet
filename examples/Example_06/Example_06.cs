31ac3804828f34f8ade154a1bb874b5b833c39e9
EDragoev <edragoev@protonmail.com>
2025-10-13 15:09:23 -0400

    Updated the example ...

--- examples/Example_06/Example_06.cs
+++ examples/Example_06/Example_06.cs
@@ -100,6 +100,7 @@ public class Example_06 {
         squareAnnotation.SetLocation(50f, 500f);
         squareAnnotation.SetSize(50f, 50f);
         squareAnnotation.SetFillColor(new float[] {0f, 0f, 1f});
+        squareAnnotation.SetTransparency(0.5f);
         squareAnnotation.SetTitle("Hello, World!");
         squareAnnotation.SetContents("The quick brown fox jumps over the lazy dog.");
         squareAnnotation.DrawOn(page);
