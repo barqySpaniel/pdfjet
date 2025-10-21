568f549f7a438678c3ed787510ae6605769b477d
EDragoev <edragoev@protonmail.com>
2025-10-20 20:20:09 -0400

    Nice cleanup.

--- net/pdfjet/TextAnnotation.cs
+++ net/pdfjet/TextAnnotation.cs
@@ -11,11 +11,5 @@ public class TextAnnotation : BaseAnnotation {
     public TextAnnotation() {
         base.annotationType = Annotation.Text;
     }
-
-//    public void Rotate(double degrees) {
-//        float[] center = container.GetRotationCenter();
-//        base.point1 = Container.RotateAroundCenter(base.point1, center, degrees);
-//        base.point2 = Container.RotateAroundCenter(base.point2, center, degrees);
-//    }
 }
 }
