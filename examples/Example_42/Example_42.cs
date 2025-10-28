ddfa05b10071b92bcc1cbb1d7373c8367387d33d
EDragoev <edragoev@protonmail.com>
2025-10-28 14:16:50 -0400

    Cleaned up the code ...

--- examples/Example_42/Example_42.cs
+++ examples/Example_42/Example_42.cs
@@ -36,34 +36,16 @@ public class Example_42 {
         fields.Add(new Field(  w/2, new String[] {"Email","jsmith12345@gmail.ca"}));
         fields.Add(new Field(   0f, new String[] {"Other Information","", ""}));
 
-/*
-        float[] xy = (new Form(fields)
-                .SetLabelFont(f1)
-                .SetLabelFontSize(8f)
-                .SetValueFont(f2)
-                .SetValueFontSize(10f)
-                .SetLocation(70f, 90f)
-                .SetRowLength(w)
-                .SetRowHeight(h)
-                .DrawOn(page));
-*/
         new Form(fields)
                 .SetLabelFont(f1)
                 .SetLabelFontSize(8f)
                 .SetValueFont(f2)
                 .SetValueFontSize(10f)
-                .SetLocation(70f, 90f)
+                .SetLocation(100f, 100f)
                 .SetRowLength(w)
                 .SetRowHeight(h)
                 .DrawOn(page);
-/*
-Console.WriteLine(xy[0]);
-Console.WriteLine(xy[1]);
-        Box box = new Box();
-        box.SetLocation(xy[0], xy[1]);
-        box.SetSize(20f, 20f);
-        box.DrawOn(page);
-*/
+
         pdf.Complete();
     }
 
