c0e72089da92bef5651a98fabc02821b99b5a73e
EDragoev <edragoev@protonmail.com>
2025-11-01 16:44:04 -0400

    Updated ...

---
+++ src/ibmplexsans/ibmplexsans.go
@@ -0,0 +1,17 @@
+package IBMPlexSans
+
+// IBMPlexSans provides the paths to the IBM Plex Sans fonts.
+const (
+	Bold             = "fonts/IBMPlexSans/IBMPlexSans-Bold.ttf.stream"
+	BoldItalic       = "fonts/IBMPlexSans/IBMPlexSans-BoldItalic.ttf.stream"
+	ExtraLight       = "fonts/IBMPlexSans/IBMPlexSans-ExtraLight.ttf.stream"
+	ExtraLightItalic = "fonts/IBMPlexSans/IBMPlexSans-ExtraLightItalic.ttf.stream"
+	Italic           = "fonts/IBMPlexSans/IBMPlexSans-Italic.ttf.stream"
+	Light            = "fonts/IBMPlexSans/IBMPlexSans-Light.ttf.stream"
+	LightItalic      = "fonts/IBMPlexSans/IBMPlexSans-LightItalic.ttf.stream"
+	Medium           = "fonts/IBMPlexSans/IBMPlexSans-Medium.ttf.stream"
+	MediumItalic     = "fonts/IBMPlexSans/IBMPlexSans-MediumItalic.ttf.stream"
+	Regular          = "fonts/IBMPlexSans/IBMPlexSans-Regular.ttf.stream"
+	SemiBold         = "fonts/IBMPlexSans/IBMPlexSans-SemiBold.ttf.stream"
+	SemiBoldItalic   = "fonts/IBMPlexSans/IBMPlexSans-SemiBoldItalic.ttf.stream"
+)
