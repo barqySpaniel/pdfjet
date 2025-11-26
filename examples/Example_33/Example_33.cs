3a0c7c4f4d8609844c4e149b102b4ef3de76f32e
EDragoev <edragoev@protonmail.com>
2025-11-25 21:53:55 -0500

    Nice cleanup ...

--- examples/Example_33/Example_33.cs
+++ examples/Example_33/Example_33.cs
@@ -10,12 +10,13 @@ public class Example_33 {
     public Example_33() {
         PDF pdf = new PDF(new BufferedStream(
                 new FileStream("Example_33.pdf", FileMode.Create)));
+
         Page page = new Page(pdf, A4.PORTRAIT);
 
         SVGImage image = new SVGImage("images/svg-test/europe.svg");
         image.SetLocation(-150f, 0f);
         float[] xy = image.DrawOn(page);
-        
+
         image = new SVGImage("images/svg/shopping_cart_checkout_FILL0_wght400_GRAD0_opsz48.svg");
         image.SetLocation(20f, 670f);
         xy = image.DrawOn(page);
@@ -33,24 +34,23 @@ public class Example_33 {
         xy = image.DrawOn(page);
 
         image = new SVGImage("images/svg/star_FILL0_wght400_GRAD0_opsz48.svg");
-        image.SetLocation(xy[0], 670);
+        image.SetLocation(xy[0], 670f);
         xy = image.DrawOn(page);
 
         image = new SVGImage("images/svg-test/test-CS.svg");
-        image.SetLocation(xy[0], 670);
+        image.SetLocation(xy[0], 670f);
         xy = image.DrawOn(page);
 
         image = new SVGImage("images/svg-test/test-QQ1.svg");
-        image.SetLocation(xy[0], 670);
+        image.SetLocation(xy[0], 670f);
         xy = image.DrawOn(page);
 
         image = new SVGImage("images/svg-test/menu-icon.svg");
-        image.SetLocation(xy[0], 670);
+        image.SetLocation(xy[0], 670f);
         xy = image.DrawOn(page);
 
         image = new SVGImage("images/svg-test/menu-icon-close.svg");
-        image.SetLocation(xy[0], 670);
-        image.ScaleBy(2.0f);
+        image.SetLocation(xy[0], 670f);
         xy = image.DrawOn(page);
 
         pdf.Complete();
--- src/examples/example33/main.go
+++ src/examples/example33/main.go
@@ -7,37 +7,20 @@ import (
 	"github.com/edragoev1/pdfjet/src/a4"
 )
 
-// Example33 -- TODO:
 func Example33() {
 	pdf := pdfjet.NewPDFFile("Example_33.pdf")
+
 	page := pdfjet.NewPage(pdf, a4.Portrait)
 
-	// SVG test
 	image := pdfjet.NewSVGImageFromFile("images/svg-test/europe.svg")
 	image.SetLocation(-150.0, 0.0)
 	xy := image.DrawOn(page)
 
-	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-CC.svg")
+	image = pdfjet.NewSVGImageFromFile("images/svg/shopping_cart_checkout_FILL0_wght400_GRAD0_opsz48.svg")
 	image.SetLocation(20.0, 670.0)
 	xy = image.DrawOn(page)
 
-	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-QQ1.svg")
-	image.SetLocation(xy[0], 670.0)
-	xy = image.DrawOn(page)
-
-	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-qt3.svg")
-	image.SetLocation(xy[0], 670.0)
-	xy = image.DrawOn(page)
-
-	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-qT4.svg")
-	image.SetLocation(xy[0], 670.0)
-	xy = image.DrawOn(page)
-
-	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-QT1.svg")
-	image.SetLocation(xy[0], 670.0)
-	xy = image.DrawOn(page)
-
-	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-qq3.svg")
+	image = pdfjet.NewSVGImageFromFile("images/svg/add_circle_FILL0_wght400_GRAD0_opsz48.svg")
 	image.SetLocation(xy[0], 670.0)
 	xy = image.DrawOn(page)
 
@@ -45,12 +28,19 @@ func Example33() {
 	image.SetLocation(xy[0], 670.0)
 	xy = image.DrawOn(page)
 
-	image = pdfjet.NewSVGImageFromFile(
-		"images/svg/shopping_cart_checkout_FILL0_wght400_GRAD0_opsz48.svg")
+	image = pdfjet.NewSVGImageFromFile("images/svg/auto_stories_FILL0_wght400_GRAD0_opsz48.svg")
+	image.SetLocation(xy[0], 670.0)
+	xy = image.DrawOn(page)
+
+	image = pdfjet.NewSVGImageFromFile("images/svg/star_FILL0_wght400_GRAD0_opsz48.svg")
 	image.SetLocation(xy[0], 670.0)
 	xy = image.DrawOn(page)
 
 	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-CS.svg")
+	image.SetLocation(xy[0], 670.0)
+	xy = image.DrawOn(page)
+
+	image = pdfjet.NewSVGImageFromFile("images/svg-test/test-QQ1.svg")
 	image.SetLocation(xy[0], 670.0)
 	xy = image.DrawOn(page)
 
@@ -60,8 +50,7 @@ func Example33() {
 
 	image = pdfjet.NewSVGImageFromFile("images/svg-test/menu-icon-close.svg")
 	image.SetLocation(xy[0], 670.0)
-	image.ScaleBy(2.0)
-	image.DrawOn(page)
+	xy = image.DrawOn(page)
 
 	pdf.Complete()
 }
